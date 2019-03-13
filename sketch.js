d7dfcdee054f1ddc227acb60cb6873eb245d7524
uaitaylor <uaitayloroliveira@gmail.com>
2019-03-13 19:58:51 -0300

    Ajuste no boneco

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
-let anguloDireito = 0;
-let anguloEsquerdo = 0;
+var anguloDireito = 0;
+var anguloEsquerdo = 0;
 
+function setup() {
+}
 
 function draw() {
-  createCanvas(500, 300);
+  createCanvas(600, 300);
   background(255);
   //Desenha Cabeça!
+  fill(255, 204, 0);
   push();
   translate(195, 60);
   rect(-25, -50, 50, 50);
-  fill(0);
   circle(0,0,4);
   pop();
   
@@ -17,7 +19,6 @@ function draw() {
   push();
   translate(195, 10);
   rect(-40, 60, 80, 100);
-  fill(0);
   circle(0,0,4);
   pop();
   
@@ -26,7 +27,6 @@ function draw() {
   translate(145, 87);
   rotate(radians(anguloDireito))
   rect(-80, -17, 80, 35);
-  fill(0);
   circle(0,0,4);
   pop();
   
@@ -35,7 +35,6 @@ function draw() {
   translate(245, 87);
   rotate(radians(anguloEsquerdo))
   rect(0, -17, 80, 35);
-  fill(0);
   circle(0,0,4);
   pop();
   
@@ -43,7 +42,7 @@ function draw() {
   push();
   translate(172, 180);
   rect(-17, 0, 35, 80);
-  fill(0);
+  fill(255, 204, 0);
   circle(0,0,4);
   pop();
   
@@ -51,29 +50,25 @@ function draw() {
   push();
   translate(217, 180);
   rect(-17, 0, 35, 80);
-  fill(0);
   circle(0,0,4);
   pop();
 
-  text("Pressione a tecla E para levantar o braço esquerdo; tecla D para levantar o braço direito", 20, height - 5);
+  fill(0);
+  text("Pressione a tecla E para levantar o braço esquerdo ou a tecla D para levantar o braço direito", 20, height - 5);
   
-
-
 }
 
 function keyPressed() {
-
-  //Balança BD
+  //Balança Braço Direito
   if (keyCode === 68 & anguloDireito == 0) {
     anguloDireito = 30;
   } else if (keyCode === 68 & anguloDireito == 30) {
     anguloDireito = 0;
   }
-  //Balnça BE 
+  //Baalnça Braço Esquerdo 
   if (keyCode === 69 & anguloEsquerdo == 0) {
     anguloEsquerdo = -30;
   } else if (keyCode === 69 & anguloEsquerdo == -30) {
     anguloEsquerdo = 0;
   }
-
 }
