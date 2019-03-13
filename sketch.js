81a4e38239306089180de214bd6c29457eef4d98
uaitaylor <uaitayloroliveira@gmail.com>
2019-03-13 19:08:32 -0300

    conclusão da tarefa

--- sketch.js
+++ sketch.js
@@ -1,79 +1,79 @@
-armAngle = 0;
-angleChange = 5;
-final = ANGLE_LIMIT = 135;
-tela = 'direito'
+let anguloDireito = 0;
+let anguloEsquerdo = 0;
 
-function setup()
-{
-  createCanvas(200, 200);
-  smooth();
-  frameRate(30);
-}
 
-function keyTyped() {
-	if (key == 'e') {
-		tela = 'esquerdo';
-	} else if (key == 'd') {
-		tela = 'direito';
-}
-}
-
-function draw()
-{
+function draw() {
+  createCanvas(500, 300);
   background(255);
+  //Desenha Cabeça!
   push();
-  translate(50, 50); // place robot so arms are always on screen
-  drawRobot();
-  armAngle += angleChange;
+  translate(195, 60);
+  rect(-25, -50, 50, 50);
+  fill(0);
+  circle(0,0,4);
+  pop();
   
+  //Desenha Tronco!
+  push();
+  translate(195, 10);
+  rect(-40, 60, 80, 100);
+  fill(0);
+  circle(0,0,4);
+  pop();
   
-  // if the arm has moved past its limit,
-  // reverse direction and set within limits.
-  if (armAngle > ANGLE_LIMIT || armAngle < 0)
-  {
-    angleChange = -angleChange;
-    armAngle += angleChange;
+  //Desenha Braço D!
+  push();
+  translate(145, 87);
+  rotate(radians(anguloDireito))
+  rect(-80, -17, 80, 35);
+  fill(0);
+  circle(0,0,4);
+  pop();
+  
+  //Desenha Braço E!
+  push();
+  translate(245, 87);
+  rotate(radians(anguloEsquerdo))
+  rect(0, -17, 80, 35);
+  fill(0);
+  circle(0,0,4);
+  pop();
+  
+  //Desenha Perna D!
+  push();
+  translate(172, 180);
+  rect(-17, 0, 35, 80);
+  fill(0);
+  circle(0,0,4);
+  pop();
+  
+  //Desenha Perna E!
+  push();
+  translate(217, 180);
+  rect(-17, 0, 35, 80);
+  fill(0);
+  circle(0,0,4);
+  pop();
+
+  text("Pressione a tecla E para levantar o braço esquerdo; tecla D para levantar o braço direito", 20, height - 5);
+  
+
+
+}
+
+function keyPressed() {
+
+  //Balança BD
+  if (keyCode === 68 & anguloDireito == 0) {
+    anguloDireito = 30;
+  } else if (keyCode === 68 & anguloDireito == 30) {
+    anguloDireito = 0;
   }
-  pop();
-}
+  //Balnça BE 
+  if (keyCode === 69 & anguloEsquerdo == 0) {
+    anguloEsquerdo = -30;
+  } else if (keyCode === 69 & anguloEsquerdo == -30) {
+    anguloEsquerdo = 0;
+  }
 
-function drawRobot()
-{
-  noStroke();
-  fill(38, 38, 200);
-  rect(20, 0, 38, 30); // head
-  rect(14, 32, 50, 50); // body
-  drawLeftArm();
-  drawRightArm();
-  pop()
-  drawPernaDireita();
-  
-  // fill(222, 222, 249);
-  // ellipse(30, 12, 12, 12); // left eye
-  // ellipse(47, 12, 12, 12);  // right eye
-}
-
-function drawLeftArm()
-{
-  push();
-  translate(8, 38);
-  rotate(radians(armAngle));
-  rect(-8, 0, 12, 37); // left arm
-  pop();
-}
-
-function drawRightArm()
-{
-  push();
-  translate(70, 38);
-  rotate(radians(-armAngle));
-  rect(-4, 0, 12, 37); // braco direito arm
-  pop();
-}
-function drawPernaDireita(){
-  translate(90,135)
-	rect(0, 0, 16, 50);
-}
-function drawPernaEsquerda(){
-	rect(22, 84, 16, 50); // left leg
 }
